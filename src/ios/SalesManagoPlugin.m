5ccb953e09ff4d890883f37842e76fa43041c6ae
Thomas Luczak <t.luczak@waigeo.fr>
2022-05-03 12:28:26 +0200

    Test iOS

--- src/ios/SalesManagoPlugin.m
+++ src/ios/SalesManagoPlugin.m
@@ -42,7 +42,37 @@
 
 - (void)finishLaunching:(NSNotification *)notification {
     NSLog(@"didFinishLaunchingWithOptions Sales Manago Plugin");
+
+    // Override point for customization after application launch.
+    [[AMMonitor sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    if (launchOptions) {
+        [[AMMonitor sharedInstance] loadPayloadForNotification:launchOptions andApplication:application loadCompletionHandlerWithError:^(AMNotification *notification, NSError *error) {
+            if (error) {
+                NSLog(@"Error occured while downloading notification :  %@", [error localizedDescription]);
+                return;
+            }
+            // implement your own logic or use default
+            [[AMMonitor sharedInstance] handleNotification:notification notificationHandler:nil dialogHandler:dialogHandler urlHandler:nil inAppHandler:nil];
+        }];
+    }
+    
+    // only for iOS > 8 (implentation for previous version omitted)
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
+    return YES;
 }
+
+void (^dialogHandler)(AMNotification *) = ^(AMNotification *notification) {
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:notification.payload[@"dialogOk"] style:UIAlertActionStyleDefault
+    handler:^(UIAlertAction *action) {
+        [[AMMonitor sharedInstance] trackNotificationCallback:notification]; 
+    }];
+    [[AMMonitor sharedInstance] displayDialog:notification withOkAction:okAction];
+};
 
 /*- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"didFinishLaunchingWithOptions Sales Manago Plugin");
