8f3948c626f633c03c1536b9ad928c124a073693
Thomas Luczak <t.luczak@waigeo.fr>
2022-05-03 12:00:20 +0200

    Test iOS

--- src/ios/SalesManagoPlugin.m
+++ src/ios/SalesManagoPlugin.m
@@ -35,6 +35,30 @@
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [[AMMonitor sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    if (launchOptions) {
+        [[AMMonitor sharedInstance] loadPayloadForNotification:launchOptions andApplication:application loadCompletionHandlerWithError:^(AMNotification *notification, NSError *error) {
+            if (error) {
+                NSLog(@"Error occured while downloading notification :  %@", [error localizedDescription]);
+                return;
+            }
+            // implement your own logic or use default
+            [[AMMonitor sharedInstance] handleNotification:notification notificationHandler:nil dialogHandler:dialogHandler urlHandler:nil inAppHandler:nil];
+        }];
+    }
+    
+    // only for iOS > 8 (implentation for previous version omitted)
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
+    return YES;
+}
+
 void (^dialogHandler)(AMNotification *) = ^(AMNotification *notification) {
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:notification.payload[@"dialogOk"] style:UIAlertActionStyleDefault
     handler:^(UIAlertAction *action) {
