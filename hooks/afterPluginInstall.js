186f63dac4da0fc5d3e4dc6605965bd0b3cf945c
Thomas Luczak <t.luczak@waigeo.fr>
2022-05-04 15:17:30 +0200

    Test iOS

--- hooks/afterPluginInstall.js
+++ hooks/afterPluginInstall.js
@@ -2,8 +2,28 @@ module.exports = function() {
     const path = require('path');
     const fse = require('fs-extra');
 
+    let libPathToTmp = path.join(__dirname, '../../../platforms/ios');
+
+    let libPathToArray = fse.readdirSync(libPathToTmp).filter(function (file) {
+        let ignore = false;
+
+        switch (file) {
+            case 'build':
+            case 'cordova':
+            case 'CordovaLib':
+            case 'platform_www':
+            case 'www':
+                ignore = true;
+            break;
+        }
+
+        return !ignore && fse.statSync(libPathToTmp + '/' + file).isDirectory();
+    });
+
     const libPathFrom = path.join(__dirname, '../libs/ios/AMMonitor.framework');
-    const libPathTo = path.join(__dirname, '../../../platforms/ios/SalesManagoTest/Plugins/cordova-plugin-salesmanago/AMMonitor.framework');
+    const libPathTo = path.join(__dirname, '../../../platforms/ios/' + libPathToArray[0] + '/Plugins/cordova-plugin-salesmanago/AMMonitor.framework');
+
+    console.log('ici', libPathTo)
 
     try {
         fse.copySync(libPathFrom, libPathTo)
